1e5e6293251ba0642619685a4c49e11ea19dd8fb
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-06 18:15:21 +0500

    navbarLinks component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-xl:h-vh-40 h-vh-26 flex flex-col justify-center items-center gap-8 border-t-2">
     <div class="flex text-2xl h-vh-17 items-center">
-     <navbarLinks />
+     <NavbarLinks />
     </div>
     <div class="text-sm text-gray-300">
       <p>Copyright &#169; 2023 Saifullah Akhtar. All Rights Reserved.</p>
@@ -11,11 +11,11 @@
 </template>
 
 <script>
-import navbarLinks from './inner/NavbarLinks.vue'
+import NavbarLinks from './inner/NavbarLinks.vue'
 export default {
     name: 'Footer',
     components:{
-        navbarLinks,
+        NavbarLinks,
     }
 }
 </script>
---
+++ src/components/inner/Humburger.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="hamburger-menu">
+        <div class="hamburger-icon" @click="toggleMenu">
+            <span class="dark:bg-white bg-stone-600"></span>
+            <span class="dark:bg-white bg-stone-600"></span>
+            <span class="dark:bg-white bg-stone-600"></span>
+        </div>
+        <div class="menu-links dark:bg-stone-600 bg-gray-600">
+            <li><a href="#about" @click="toggleMenu">About</a></li>
+            <li><a href="#experience" @click="toggleMenu">Experience</a></li>
+            <li><a href="#projects" @click="toggleMenu">Projects</a></li>
+            <li><a href="#contact" @click="toggleMenu">Contact</a></li>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Humburger',
+    methods: {
+            toggleMenu() {
+                const menu = document.querySelector(".menu-links");
+                const icon = document.querySelector(".hamburger-icon");
+                menu.classList.toggle("open");
+                icon.classList.toggle("open");
+            }
+        }
+}
+</script>
+
+<style>
+#hamburger-nav {
+    display: none;
+}
+
+.hamburger-menu {
+    border-radius: 20px;
+    position: relative;
+    display: inline-block;
+}
+
+.hamburger-icon {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 24px;
+    width: 30px;
+    cursor: pointer;
+}
+
+.hamburger-icon span {
+    width: 100%;
+    height: 2px;
+    /* background-color: white; */
+    transition: all 0.3 ease-in-out;
+}
+
+.menu-links {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    border: 2px;
+    border-color: antiquewhite;
+    border-radius: 24px;
+    /* background-color: rgb(36, 35, 35); */
+    width: fit-content;
+    max-height: 0;
+    overflow: hidden;
+    transition: all 0.3 ease-in-out;
+}
+
+.menu-links a {
+    display: block;
+    padding: 10px;
+    text-align: center;
+    font-size: 1.5rem;
+    color: white;
+    text-decoration: none;
+    transition: all 0.3 ease-in-out;
+}
+
+.menu-links li {
+    list-style: none;
+}
+
+.menu-links.open {
+    max-height: 300px;
+}
+
+.hamburger-icon.open span:first-child {
+    transform: rotate(45deg) translate(10px, 5px);
+}
+
+.hamburger-icon.open span:nth-child(2) {
+    opacity: 0;
+}
+
+.hamburger-icon.open span:last-child {
+    transform: rotate(-45deg) translate(10px, -5px);
+}
+
+.hamburger-icon span:first-child {
+    transform: none;
+}
+
+.hamburger-icon span:first-child {
+    opacity: 1;
+}
+
+.hamburger-icon span:first-child {
+    transform: none;
+}
+</style>
