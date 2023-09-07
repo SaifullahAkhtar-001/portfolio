5af85da6fe468f98b77e894910f4ba096a38002c
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-07 18:02:43 +0500

    hello

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,13 @@
       <Profile id="profile" />
       <Aboutme id="about" />
       <Footer />
+<<<<<<< Updated upstream
       <a href="#home" class="z-[2000] transition-all max-xl:duration-1000 duration-100 flex justify-center items-center bg-opacity-90 rounded-full fixed bottom-8 right-4 h-10 w-10 cursor-pointer border-2 border-stone-400 active:border-2 xl:hover:border-2  active:border-green-500 xl:hover:border-green-500">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 dark:fill-white fill-stone-400" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
       </a>
+=======
+      <homebtn/>
+>>>>>>> Stashed changes
   </div>
 </template>
 
@@ -15,11 +19,12 @@ import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 import Profile from './components/Profile.vue'
 import Aboutme from './components/Aboutme.vue'
+import homebtn from './components/homebtn.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar, Footer,Profile,Aboutme,
+    Navbar, Footer,Profile,Aboutme,homebtn,
   }
 }
 </script>
