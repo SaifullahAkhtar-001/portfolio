38793a78bbc038b3863fcff8c6bec9d0eea8fbc4
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-07 21:19:57 +0500

    responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-vh-17 flex max-xl:ml-8 justify-center items-center text-2xl">
+    <div class="h-vh-17 flex max-xl:ml-8 max-xl:justify-center xl:justify-around items-center text-2xl">
         <Humburger class="xl:hidden"/>
         <div class="text-3xl">
             Saifullah Akhtar
