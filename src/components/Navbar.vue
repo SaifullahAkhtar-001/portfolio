8dc2ea7aa58a42da4e597a8be92b6816fd9699bf
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-07 20:47:28 +0500

    humberger

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="h-vh-17 flex max-xl:ml-8 xl:justify-around items-center text-2xl">
+    <div class="h-vh-17 flex max-xl:ml-8 justify-center items-center text-2xl">
+        <Humburger class="xl:hidden"/>
         <div class="text-3xl">
             Saifullah Akhtar
         </div>
         <NavbarLinks class="max-xl:hidden relative" />
-        <Humburger class="xl:hidden"/>
     </div>
 </template>
 
--- src/components/inner/Humburger.vue
+++ src/components/inner/Humburger.vue
@@ -1,113 +1,27 @@
 <template>
-    <div id="menu">
-        <div id="menu-bar" @click="menuOnClick">
-            <div id="bar1" class="bar dark:bg-white bg-stone-900"></div>
-            <div id="bar2" class="bar dark:bg-white bg-stone-900"></div>
-            <div id="bar3" class="bar dark:bg-white bg-stone-900"></div>
-        </div>
-        <nav class="nav" id="nav">
-            <ul class="bg-[#5c30b1] mr-4 rounded-[2rem] cast-shadow">
-                <li><a href="#about" @click="menuOnClick">About</a></li>
-                <li><a href="#experience" @click="menuOnClick">Experience</a></li>
-                <li><a href="#projects" @click="menuOnClick">Projects</a></li>
-                <li><a href="#contact" @click="menuOnClick">Contact</a></li>
-            </ul>
-        </nav>
+    <div id="menuToggle">
+        <input type="checkbox" />
+        
+            <span class="dark:bg-white bg-stone-800"></span>
+            <span class="dark:bg-white bg-stone-800"></span>
+            <span class="dark:bg-white bg-stone-800"></span>
+
+        <ul id="menu" class="dark:text-white text-black">
+            <li class="transition-all hover:opacity-60"><a href="#about" @click="menuOnClick">About</a></li>
+            <li class="transition-all hover:opacity-60"><a href="#experience" @click="menuOnClick">Experience</a></li>
+            <li class="transition-all hover:opacity-60"><a href="#projects" @click="menuOnClick">Projects</a></li>
+            <li class="transition-all hover:opacity-60"><a href="#contact" @click="menuOnClick">Contact</a></li>
+
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Humburger',
-    methods: {
-        menuOnClick() {
-            document.getElementById("menu-bar").classList.toggle("change");
-            document.getElementById("nav").classList.toggle("change");
-        }
-    }
 }
 </script>
 
 <style>
-
-#menu {
-    z-index: 2;
-}
-
-#menu-bar {
-    width: 32px;
-    height: 35px;
-    margin: 58px 20px 20px 20px;
-    cursor: pointer;
-}
-.cast-shadow{
-    box-shadow: 0 10px 20px rgba(92, 48, 177, 0.7);
-}
-.bar {
-    height: 3px;
-    width: 100%;
-    /* background-color: #3586ff; */
-    display: block;
-    border-radius: 5px;
-    transition: 0.3s ease;
-}
-
-#bar1 {
-    transform: translateY(-4px);
-}
-
-#bar3 {
-    transform: translateY(4px);
-}
-
-.nav {
-    transition: 0.3s ease;
-    display: none;
-}
-
-.nav ul {
-    padding: 0 22px;
-}
-
-.nav li {
-    list-style: none;
-    padding: 12px 0;
-}
-
-.nav li a {
-    color: white;
-    font-size: 20px;
-    text-decoration: none;
-}
-
-.nav li a:hover {
-    font-weight: bold;
-}
-
-#menu {
-    top: 0;
-    right: 0;
-    position: absolute;
-}
-
-.change {
-    display: block;
-}
-
-.change .bar {
-    background-color: white;
-}
-
-.change #bar1 {
-    transform: translateY(2px) rotateZ(-51deg);
-}
-
-.change #bar2 {
-    opacity: 0;
-}
-
-.change #bar3 {
-    transform: translateY(-4px) rotateZ(45deg);
-}
 
 </style>
