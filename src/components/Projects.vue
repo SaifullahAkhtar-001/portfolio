17cfd9715023f6c85c5268012500e6a295ed38ee
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-09 00:54:16 +0500

    add images

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,9 +3,9 @@
     <div class="flex flex-col justify-center gap-10 items-center h-[80vh] max-xl:h-fit">
       <Sectionheader heading="Projects" subheading="Browse My Recent" />
       <div class="flex flex-row max-md:flex-wrap gap-6 items-center justify-center">
-        <Projectcard url="../../assets/project-1.png" projName="Blog App" live="https://blog-app-production-af82.up.railway.app/" repo="https://github.com/SaifullahAkhtar-001/Blog-App"/>
-        <Projectcard url="../../assets/project2.png" projName="Elite Watches" live="https://elite-watches.vercel.app/" repo="https://github.com/SaifullahAkhtar-001/elite-watches "/>
-        <Projectcard url="../../assets/project3.png" projName="Weather App" live="https://saifullahakhtar-001.github.io/WeatherApp/" repo="https://github.com/SaifullahAkhtar-001/WeatherApp"/>        
+        <Projectcard url="/img/project1.b5e3389d.png" projName="Blog App" live="https://blog-app-production-af82.up.railway.app/" repo="https://github.com/SaifullahAkhtar-001/Blog-App"/>
+        <Projectcard url="/img/project2.5aa82cb5.png" projName="Elite Watches" live="https://elite-watches.vercel.app/" repo="https://github.com/SaifullahAkhtar-001/elite-watches "/>
+        <Projectcard url="/img/project3.79560941.png" projName="Weather App" live="https://saifullahakhtar-001.github.io/WeatherApp/" repo="https://github.com/SaifullahAkhtar-001/WeatherApp"/>        
       </div>
     </div>
   </Section>
--- src/components/inner/Projectcard.vue
+++ src/components/inner/Projectcard.vue
@@ -2,7 +2,7 @@
     <div
         class="flex flex-col items-center justify-center gap-4 p-6 border-[1px] border-solid border-stone-600 rounded-[2rem] shadow-2xl">
         <div class="w-[90%]">
-            <img :src="url" alt="hello mf" class="rounded-[1.5rem]">
+            <img :src="url" alt="hello " class="rounded-[1.5rem]">
         </div>
         <div class="font-semibold">
             {{ projName }}
@@ -37,10 +37,6 @@ export default {
         navigateToRepo() {
             window.location.href = this.repo;
         },
-    },
-    created() {
-        // Log something when the component is created
-        console.log(this.url);
     },
 }
 </script>
