8d054333355177080e9633308102a413e93a423a
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-08 19:52:36 +0500

    Update Experience.vue

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,6 +1,6 @@
 <template>
     <Section>
-        <div class="h-[16vh] max-xl:h-[80vh]">
+        <div class="h-[16vh] max-md:h-[70vh]">
             <div class="text-center font-extralight text-base opacity-75 mb-3">Explore</div>
             <div class="font-medium text-5xl text-center">My Experience</div>
         </div>
