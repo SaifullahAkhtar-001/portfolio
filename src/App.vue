3c513c5cbf8a84ff1d37820f71a86daeee0c6b86
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-08 20:57:53 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@
       <Navbar id="home" />
       <Profile id="profile" />
     </div>
-    <div class="dark:bg-[url('../assets/color-sharp.png')] dark:bg-no-repeat dark:bg-cover rotate-180">
-      <div class="dark:bg-[url('../assets/color-sharp2.png')] dark:bg-no-repeat dark:bg-cover rotate-180">
-        <Aboutme id="about" />
-      </div>
+    <div class="dark:bg-[url('../assets/banner-bg.png')] dark:bg-no-repeat dark:bg-cover">
+      <Aboutme id="about" />
     </div>
     <div class="dark:bg-[url('../assets/color-sharp.png')] dark:bg-no-repeat dark:bg-cover rotate-180">
-      <Experience class="rotate-180"/>
+      <Experience class="rotate-180" />
     </div>
     <Footer />
     <homebtn />
