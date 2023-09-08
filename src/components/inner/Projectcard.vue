faa85c1641a3fe1a3d5e3f2375113922a8895892
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-09 00:28:18 +0500

    check

--- src/components/inner/Projectcard.vue
+++ src/components/inner/Projectcard.vue
@@ -2,20 +2,22 @@
     <div
         class="flex flex-col items-center justify-center gap-4 p-6 border-[1px] border-solid border-stone-600 rounded-[2rem] shadow-2xl">
         <div class="w-[90%]">
-            <img src="../../assets/project-1.png" alt="" class="rounded-[1.5rem]">
+            <img :src="url" alt="hello mf" class="rounded-[1.5rem]">
         </div>
         <div class="font-semibold">
-            Project-1
+            {{ projName }}
         </div>
         <div>
             <div class="max-sm:flex-col flex gap-4 text-sm font-bold">
-                <button
-                    class="transition-colors duration-200 px-6 py-4 border rounded-full dark:hover:bg-white hover:bg-black dark:hover:text-black hover:text-white">
-                    Download CV
+                <button @click="navigateToLive"
+                    class="transition-colors duration-200 px-6 py-4 border rounded-full dark:hover:bg-white hover:bg-black dark:hover:text-black hover:text-white"
+                    target="_blank">
+                    Live
                 </button>
-                <button
-                    class="transition-colors duration-200 px-6 py-4 border rounded-full bg-stone-800 text-white dark:bg-white dark:text-stone-800 dark:hover:bg-stone-800 hover:bg-white dark:hover:text-white hover:text-stone-800">
-                    Contact Info
+                <button @click="navigateToRepo"
+                    class="transition-colors duration-200 px-6 py-4 border rounded-full bg-stone-800 text-white dark:bg-white dark:text-stone-800 dark:hover:bg-stone-800 hover:bg-white dark:hover:text-white hover:text-stone-800"
+                    target="_blank">
+                    Repo
                 </button>
             </div>
         </div>
@@ -24,7 +26,22 @@
 
 <script>
 export default {
-    name: 'Projectcard'
+    name: 'Projectcard',
+    props: [
+        'url', 'projName', 'live', 'repo'
+    ],
+    methods: {
+        navigateToLive() {
+            window.location.href = this.live;
+        },
+        navigateToRepo() {
+            window.location.href = this.repo;
+        },
+    },
+    created() {
+        // Log something when the component is created
+        console.log(this.url);
+    },
 }
 </script>
 
