02672eb98e1602568e7b3afd5bd795dc40a6466c
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-09-11 18:14:44 +0500

    ok

--- src/components/inner/Projectcard.vue
+++ src/components/inner/Projectcard.vue
@@ -2,18 +2,19 @@
     <div
         class="flex flex-col items-center justify-center gap-4 p-6 border-[1px] border-solid border-stone-600 rounded-[2rem] shadow-2xl">
         <div class="w-[90%]">
-            <img :src="url" alt="hello " class="rounded-[1.5rem]">
+            <img src="/img/project1.b5e3389d.png" alt="hello " class="rounded-[1.5rem]">
         </div>
-        <div class="font-semibold">
+        <div class="font-medium">
             {{ projName }}
         </div>
         <div>
+            
             <div class="max-sm:flex-col flex gap-4 text-sm font-bold">
-                <button @click="navigateToLive"
+                <router @click="navigateToLive"
                     class="transition-colors duration-200 px-6 py-4 border rounded-full dark:hover:bg-white hover:bg-black dark:hover:text-black hover:text-white"
                     target="_blank">
                     Live
-                </button>
+                </router>
                 <button @click="navigateToRepo"
                     class="transition-colors duration-200 px-6 py-4 border rounded-full bg-stone-800 text-white dark:bg-white dark:text-stone-800 dark:hover:bg-stone-800 hover:bg-white dark:hover:text-white hover:text-stone-800"
                     target="_blank">
@@ -25,8 +26,15 @@
 </template>
 
 <script>
+import { IMAGE1, IMAGE2, IMAGE3 } from '@/config/urls';
 export default {
+
     name: 'Projectcard',
+    data() {
+        return{
+            img1: IMAGE2,
+        }
+    },
     props: [
         'url', 'projName', 'live', 'repo'
     ],
